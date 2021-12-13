2b919955c70dfe09fdec771812319c2047a8b721
sahibmartial <sahibmartial@gmail.com>
2021-12-13 10:31:05 +0100

    User create

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', methods: ['GET'], name: 'home')]
+    public function index(): Response
+    {
+        //page accueil search et create blog after user is auth
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
